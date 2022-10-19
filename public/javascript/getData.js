2bc8e1c7b31f4f061f5d66481c8c9a5a14e834e7
iFaaka <alvarezfacundo321@gmail.com>
2022-10-19 17:24:59 -0300

    adding corse

--- public/javascript/getData.js
+++ public/javascript/getData.js
@@ -1,18 +1,15 @@
 //Function to get all the items
 
 async function getData(element, func) {
-  /*   const corsAnywhere = "https://cors-anywhere.herokuapp.com/"; */
-
   const url = `https://bs-ba-fa.herokuapp.com/${element}`;
 
-  /*   fetch(corsAnywhere + yourUrl, {
+  fetch(url, {
     method: "GET",
     headers: new Headers({
       "Content-Type": "application/json",
       "Access-Control-Allow-Origin": "*",
     }),
-  }) */
-  fetch(url)
+  })
     .then((response) => response.json())
     .then((data) => func(data))
     .catch((err) => console.log(err));
