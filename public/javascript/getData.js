bd378ce5769e846befa133d1b331cdf3fd0df4e4
iFaaka <alvarezfacundo321@gmail.com>
2022-10-19 17:21:23 -0300

    Wihout cors

--- public/javascript/getData.js
+++ public/javascript/getData.js
@@ -1,17 +1,18 @@
 //Function to get all the items
 
 async function getData(element, func) {
-  const corsAnywhere = "https://cors-anywhere.herokuapp.com/";
+  /*   const corsAnywhere = "https://cors-anywhere.herokuapp.com/"; */
 
-  const yourUrl = `https://bs-ba-fa.herokuapp.com/${element}`;
+  const url = `https://bs-ba-fa.herokuapp.com/${element}`;
 
-  fetch(corsAnywhere + yourUrl, {
+  /*   fetch(corsAnywhere + yourUrl, {
     method: "GET",
     headers: new Headers({
       "Content-Type": "application/json",
       "Access-Control-Allow-Origin": "*",
     }),
-  })
+  }) */
+  fetch(url)
     .then((response) => response.json())
     .then((data) => func(data))
     .catch((err) => console.log(err));
